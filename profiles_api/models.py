c8d5f883ab278020848cdc701001eed298dd8755
Alan Mejia <alan-mejia@outlook.com>
2023-07-08 22:11:12 -0600

    enabled django admin and superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,8 +8,8 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    # functions specified in this class are used to manipulate objects within 
-    # the model that manager is for, the create_user and create_superuser will be 
+    # functions specified in this class are used to manipulate objects within
+    # the model that manager is for, the create_user and create_superuser will be
     # called by the manager cli when creating users
     def create_user(self, email, name, password=None):
         if not email:
@@ -21,18 +21,17 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
         user.set_password(password)
         # its best practise to indicate the database we are going to use
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
-    
     def create_superuser(self, email, name, password):
         """create superuser with give details"""
         user = self.create_user(email=email, name=name, password=password)
         # super user atribb is created by the permissions mixin
         user.is_superuser = True
         user.is_staff = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
