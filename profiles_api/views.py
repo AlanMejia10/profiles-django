18c296ff6a75139c94e9df6cfef760770fd1f617
Alan Mejia <alan-mejia@outlook.com>
2023-07-09 13:17:33 -0600

    added post and get http verbs

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView"""
@@ -17,3 +20,19 @@ class HelloApiView(APIView):
             'message': "Hello!",
             'an_appview': an_appview
         })
+
+    def post(self, request):
+        """create a hello message"""
+        # passes the data from the request to the serializer obj
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"hello {name}"
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
