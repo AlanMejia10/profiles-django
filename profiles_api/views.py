48441e646ca0adcedaae69ea594c0e2771b65862
Alan Mejia <alan-mejia@outlook.com>
2023-07-26 09:11:32 -0600

    modified urls and views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -66,4 +69,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
     permission_classes = (permissions.UpdateOwnProfile, )
-    
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """this allow us to view the login, by default is not rendered"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
